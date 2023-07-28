4f6b6126a197244a11a1cfebd481a1ac5c89ed3d
BACK3R17 <spbekker6@gmail.com>
2023-07-28 16:19:04 +0200

    Initial

---
+++ DAL/Models.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    public class BookmarkContext : DbContext
+    {
+        public DbSet<Bookmark> Bookmarks { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Keyword> Keywords { get; set; }
+    }
+
+    public class Bookmark
+    {
+        public int BookmarkID { get; set; }
+        public string BookmarkName { get; set; }
+        public int CategoryID { get; set; }
+        public int LanguageID { get; set; }
+        public DateTime BookmarkDateAdded { get; set; } = DateTime.Now;
+
+    }
+
+    public class Language
+    {
+        public int LanguageID { get; set; }
+        public string LanguageName { get; set; }
+        public DateTime LanguageDateAdded { get; set; } = DateTime.Now;
+
+    }
+
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public int CategoryName { get; set; }
+        public DateTime CategoryDateAdded { get; set; } = DateTime.Now;
+
+    }
+
+    public class Keyword
+    {
+        public int KeywordID { get; set; }
+        public string KeywordName { get; set; }
+        public int BookmarkID { get; set; }
+        public DateTime KeywordDateAdded { get; set; } = DateTime.Now;
+
+    }
+}
